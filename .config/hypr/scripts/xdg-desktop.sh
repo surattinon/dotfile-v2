bbf4ed9caa8511146de61218ab7fbe9467ec5d99
surattinon <surattinon.basu@gmail.com>
2026-02-03 01:44:06 +0700

    update configs

--- .config/hypr/scripts/xdg-desktop.sh
+++ .config/hypr/scripts/xdg-desktop.sh
@@ -1,8 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/sh
 sleep 1
 killall -e xdg-desktop-portal-hyprland
 killall xdg-desktop-portal
-killall xdg-desktop-portal-gtk
 /usr/lib/xdg-desktop-portal-hyprland &
 sleep 2
 /usr/lib/xdg-desktop-portal &
